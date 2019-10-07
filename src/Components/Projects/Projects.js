d88abe1f9b681da5d04edbe1a09753f35d70d9bc
Jacob Goskha <jacobgoskha@gmail.com>
2019-10-07 17:22:50 -0400

    projects design

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -2,16 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Projects.css';
 import tenor from '../../gif/tenor.gif';
+import appDemo from '../../img/pubgAppDemo.PNG';
 
 const Projects = props => {
   return (
     <div className='projectsSection'>
       <h2>PROJECTS</h2>
       <div className='pubgStatsProject' data-aos='fade-left'>
-        <div className='layerOnProjectPubg'>layer</div>
+        <div className='layerOnProjectPubg'></div>
         <p className='projectTitle'>PUBG stats application</p>
 
-        <p>Front end live demo: Back end server Node JS</p>
+        <p className='projectLiveDemoLink'>
+          <a href='https://pubg-stats-app.netlify.com'>Live demo</a>
+        </p>
       </div>
       <div className='app2' data-aos='fade-right'>
         App #2
