abcbb37883610110b8ff04aab9ed4171867b3b16
Jacob Goskha <jacobgoskha@gmail.com>
2019-10-07 16:19:19 -0400

    Projects design

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -8,8 +8,10 @@ const Projects = props => {
     <div className='projectsSection'>
       <h2>PROJECTS</h2>
       <div className='pubgStatsProject' data-aos='fade-left'>
-        <div className='layerOnProject'>layer</div>
-        <p>PUBG stats application</p>
+        <div className='layerOnProjectPubg'>layer</div>
+        <p className='projectTitle'>PUBG stats application</p>
+
+        <p>Front end live demo: Back end server Node JS</p>
       </div>
       <div className='app2' data-aos='fade-right'>
         App #2
